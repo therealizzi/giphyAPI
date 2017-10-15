470a1ba97261dbbe0cd54ba7903d74aa659a1408
TheRealIzzi <israel.peck@gmail.com>
2017-10-14 20:08:57 -0500

    Fixed more bugs -B still needs pause effect and some html ~85%

--- Assets/logic.js
+++ Assets/logic.js
@@ -2,14 +2,18 @@
 //Need to declare variables:
 
 var newBtn = "";
-var newGif = "";
+var newGifd = "";
+var newGifs = "";
 var postGif = "";
 var makeBtn = "";
 var length = 10;
-var apiKey = "dc6zaTOxFJmzC";
+var apiKey = "";
 var topics = ["Mario","Luigi","Bowser"]
-var topic = "cats"
-var callURL = "http://api.giphy.com/v1/gifs/search?q="+topic+"&api_key="+apiKey+"&limit="+length
+var topic = "bats"
+var callURLa = "http://api.giphy.com/v1/gifs/search?q="
+var callURLb = "&api_key=dc6zaTOxFJmzC&limit=10"
+var callURL = "http://api.giphy.com/v1/gifs/search?q="+topic+"&api_key=dc6zaTOxFJmzC&limit=10"
+
 
 //Need a loop to append buttons for each string in topics[]
 
@@ -29,18 +33,26 @@ $(document).ready(function(){
 
   $(document).on('click','.getgifs',function(event){
 
+    //Need to get the id of the button and use it to find the topic to search
+    topic = $(this).attr("id")
+
+    //Need an ajax call to the URL to pull in gifs
     $.ajax({
-      url : callURL,
+      url : callURLa+topic+callURLb,
       method : "GET"
     }).done(function(response){
 
+      console.log(response)
+
+    //NEed to clear the existing gifs to make room for the new ones
     $("#apndGifHere").html("");
 
       for (i = 0; i < length; i++) {
 
-        newGif = response.data[i].images.fixed_height.url;
+        newGifd = response.data[i].images.fixed_height_small.url;
+        newGifs = response.data[i].images.fixed_height_small_still.url;
         postGif = $("<img>");
-        postGif.attr("src", newGif);
+        postGif.attr("src", newGifs);
         postGif.attr("alt", "a silly gif");
         $("#apndGifHere").prepend(postGif);
 
@@ -68,5 +80,22 @@ $(document).ready(function(){
     newBtn = $("#btnValue").val();
     createBtn()
   });
+
+  $(".gifRating").on("click", function(){
+
+    var state = $(this).attr("data-state")
+
+        console.log(state)
+
+    if (state === "still"){
+      $(this).attr("src", $(this).attr(newGifd))
+      $(this).attr("data-state","animate")
+    } else {
+      $(this).attr("src",$(this).attr(newGifs))
+      $(this).attr("data-state","still")
+    }
+
+  })
+
 
 });
