48e7292440ac66b3761dae5022312f84eafaeb7f
TheRealIzzi <israel.peck@gmail.com>
2017-10-18 15:26:14 -0500

    Fixed all bugs in logic; all features working ~97%

--- Assets/logic.js
+++ Assets/logic.js
@@ -1,21 +1,23 @@
 
-//Need to declare variables:
-
+//Need to declare global variables:
 var newBtn = "";
 var postGif = "";
 var makeBtn = "";
 var length = 10;
 var apiKey = "";
-var topics = ["Princess Peach","Koopa","Toad","Wario","Yoshi","Mario","Luigi","Bowser"]
-var topic = ""
-var callURLa = "http://api.giphy.com/v1/gifs/search?q="
-var callURLb = "&api_key=dc6zaTOxFJmzC&limit=10"
-var callURL = "http://api.giphy.com/v1/gifs/search?q="+topic+"&api_key=dc6zaTOxFJmzC&limit=10"
+var response = "";
+var topics = ["Princess Peach","Koopa","Toad","Wario","Yoshi","Mario","Luigi","Bowser"];
+var topic = "";
+var callURLa = "http://api.giphy.com/v1/gifs/search?q=";
+var callURLb = "&api_key=dc6zaTOxFJmzC&limit=10&rating=pg";
 
-
-//Need a loop to append buttons for each string in topics[]
-
+//This starts the program, once the DOM is loaded
 $(document).ready(function(){
+  displayButtons()
+  });
+
+//This displays the buttons on the screen
+function displayButtons(){
 
   for (i = 0; i < topics.length; i++) {
     var makeBtn = $("<button>");
@@ -23,82 +25,89 @@ $(document).ready(function(){
     makeBtn.attr("id", topics[i]);
     makeBtn.attr("class", "getgifs");
     $("#apndBtnHere").prepend(makeBtn); 
-  }
-
-  //Need an onclick event to trigger ajax call and pull data and assign to var newGif
-
-  console.log(callURL)
-
-  $(document).on('click','.getgifs',function(event){
-
-    //Need to get the id of the button and use it to find the topic to search
-    topic = $(this).attr("id")
-
-    //Need an ajax call to the URL to pull in gifs
-    $.ajax({
-      url : callURLa+topic+callURLb,
-      method : "GET"
-    }).done(function(response){
-
-      console.log(response)
-
-    //Need to clear the existing gifs to make room for the new ones
-    $("#apndGifHere").html("");
-
-      for (i = 0; i < length; i++) {
-
-        var appenderDiv = $("<div>");
-
-        appenderDiv.attr("class", "apndDiv");
-
-        $("#apndGifHere").prepend(appenderDiv)
-
-        newRating = response.data[i].rating;
-        postRating = $("<div>");
-        postRating.html("Rated: "+newRating);
-        postRating.attr("class", "gifRating");
-        appenderDiv.append(postRating);
-
-        newGifs = response.data[i].images.fixed_height_still.url;
-        postGif = $("<img>");
-        postGif.attr("src", newGifs);
-        postGif.attr("alt", "a silly gif");
-        postGif.attr("data-state", "still");
-        postGif.attr("class", "postGif");
-        appenderDiv.append(postGif);
-      }
-    });
-  });
-
-  //Need a "createBtn()" function to generate buttons
-
-  function createBtn(){
-    var makeBtn = $("<button>"+newBtn+"</button>");
-    makeBtn.attr("id",newBtn)
-    makeBtn.attr("class", "getgifs")
-    $("#apndBtnHere").prepend(makeBtn);
   };
+}; 
 
-  //Need a button on('click') function to store the input value in a btnValue variable and trigger next function "createBtn()"
+//This pulls gifs from the url, based on clicks
+$(document).on('click','.getgifs', function(event){
+
+  //This gets the id of the button pressed
+  topic = $(this).attr("id");
+
+  //This calls the API for the id of the button ("topic")
+  function getData(yt_url, callback) {
+    $.ajax({
+      url : yt_url,
+      method : "GET",
+      success: callback,
+    });
+  }
   
-  $(".newBtn").click(function(){
-    newBtn = $("#btnValue").val();
-    createBtn()
+  //This syncs api results with the global 'response' variable
+  getData(callURLa+topic+callURLb, function(output){
+    window.response = output;
+    apndGif();
   });
+});
 
-  $(this).on("click",".postGif", function(){
+//This appends gifs to the DOM
+function apndGif(){
 
-    var state = $(this).attr("data-state")
+  //This clears the existing gifs to make room for the new ones
+  $("#apndGifHere").html("");
 
-    var src = $(this).attr("src")
+    //This loops through the display object 
+    for (i = 0; i < length; i++) {
 
-    if (state === "still"){
-      $(this).attr("src", src)
-      $(this).attr("data-state","animate")
-    } else {
-      $(this).attr("src", src.replace('_s',''))
-      $(this).attr("data-state","still")
-    }
-  });
+      //This creates jquery HTML to display the object
+      var appenderDiv = $("<div>");
+      appenderDiv.attr("class", "apndDiv");
+      $("#apndGifHere").prepend(appenderDiv)
 
+      //This selects which gif to display and wraps it in an 'img' tag
+      newGifs = response.data[i].images.fixed_height_small_still.url;
+      postGif = $("<img>");
+      postGif.attr("src", newGifs);
+      postGif.attr("alt", "a silly gif");
+      postGif.attr("data-state", "still");
+      postGif.attr("class", "postGif");
+      appenderDiv.append(postGif);
+
+      //This selects which rating to display and wraps it in a 'div' tag
+      newRating = response.data[i].rating;
+      postRating = $("<div>");
+      postRating.html("Rated: "+newRating);
+      postRating.attr("class", "gifRating");
+      appenderDiv.append(postRating);
+    };
+};
+
+//This makes new buttons
+function createBtn(){
+  var makeBtn = $("<button>"+newBtn+"</button>");
+  makeBtn.attr("id",newBtn)
+  makeBtn.attr("class", "getgifs")
+  $("#apndBtnHere").prepend(makeBtn);
+};
+
+//This takes the user input and stores the value
+$(".newBtn").click(function(){
+  newBtn = $("#btnValue").val();
+  topics.push(newBtn);
+  createBtn();
+});
+
+//This changes the data-state of the gif
+$(document).on("click",".postGif", function(){
+
+  var state = $(this).attr("data-state");
+  var src = $(this).attr("src");
+
+  if (state === "still"){
+    $(this).attr("src", src.replace('100_s','100'))
+    $(this).attr("data-state","animate")
+  } else {
+    $(this).attr("src", src.replace('100','100_s'));
+    $(this).attr("data-state","still")
+  }
 });
